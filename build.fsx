e77670528a9aa8d67cd018bd99c030cfefbcd1f7
theburningmonk <theburningmonk@gmail.com>
2014-01-13 02:31:22 +0000

    - fixed build script

--- build.fsx
+++ build.fsx
@@ -68,7 +68,8 @@ Target "AssemblyInfo" (fun _ ->
         Attribute.Product       project
         Attribute.Description   summary
         Attribute.Version       release.AssemblyVersion
-        Attribute.FileVersion   release.AssemblyVersion ] 
+        Attribute.FileVersion   release.AssemblyVersion
+        Attribute.InternalsVisibleTo "SimpleSpeedTester.Tests" ] 
 )
 
 // --------------------------------------------------------------------------------------
